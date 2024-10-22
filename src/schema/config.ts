41f70a636d0ba5eab25e2a5fba81b1e419f1ca78
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-10-21 22:35:40 -0500

    Remove config schema version field

--- src/schema/config.ts
+++ src/schema/config.ts
@@ -2,12 +2,12 @@ import z from "zod";
 
 const ConfigSchema = z
   .object({
-    version: z.string(),
     forceTheme: z
       .enum(["light", "dark"])
       .describe("Force override the theme used while in fallback mode")
       .optional(),
   })
-  .describe("Customize the settings for customizing the Apatite instance.");
+  .describe("Customize the settings for customizing the Apatite instance.")
+  .optional();
 
-  export default ConfigSchema;
+export default ConfigSchema;
