8d95f5855760addc72ab2e36f7ce98ed167ccc7f
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-04 18:27:33 -0600

    feat: implement filtering by date range

--- src/scripts/processor.ts
+++ src/scripts/processor.ts
@@ -1,17 +1,34 @@
 import { Schedule } from "../schema/schedule";
 import { schedules } from "./fetcher";
 
-// Function to print date range for all schedules if available
-function printDateRanges() {
-  schedules.forEach((schedule: Schedule) => {
-    if (schedule.dateRangeValid) {
-      const { start, end } = schedule.dateRangeValid;
-      console.log(`Start Date: ${start}, End Date: ${end}`);
-    } else {
-      console.log("No date range available.");
-    }
+/**
+ * Filters the schedules based on their date range validity.
+ *
+ * This function iterates over the list of schedules and applies the following filters:
+ * - If the schedule does not have a valid date range, it is included.
+ * - If the schedule has a start date and it is in the future, it is excluded.
+ * - If the schedule has an end date and it is in the past, it is excluded.
+ *
+ * @returns {Schedule[]} The filtered list of schedules.
+ */
+function dateRangeFilter(): Schedule[] {
+  return schedules.filter((schedule: Schedule) => {
+    if (!schedule.dateRangeValid) return true;
+
+    // Assign variables only if the dates exist
+    const startDate = schedule.dateRangeValid.start
+      ? new Date(schedule.dateRangeValid.start + "T00:00:00")
+      : null;
+    const endDate = schedule.dateRangeValid.end
+      ? new Date(schedule.dateRangeValid?.end + "T00:00:00")
+      : null;
+
+    // Filter out schedules that have not started yet
+    if (startDate && startDate > new Date()) return false;
+
+    // Filter out schedules that have ended
+    if (endDate && endDate < new Date()) return false;
+
+    return true;
   });
 }
-
-// Call the function to print date ranges for all schedules
-printDateRanges();
