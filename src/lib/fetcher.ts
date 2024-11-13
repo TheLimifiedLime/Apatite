5cd4f9486760800822c5c38e46d2b94e8754c5cb
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-12 23:42:40 -0600

    refactor: minor refactoring

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,57 +1,11 @@
-import { ScheduleSchema, Schedule } from "../schema/schedule";
-import { ConfigSchema, Config } from "../schema/config";
-
-const urlParameters = new URLSearchParams(window.location.search);
-
-/**
- * Fetches and validates schedules from the URL parameters.
- * @returns {Schedule[]} An array of schedules passed to Apatite.
- */
-function fetchSchedules(): Schedule[] {
-  // Get value of "schedules" query parameter
-  const schedulesParameter = urlParameters.get("schedules");
-  if (!schedulesParameter) {
-    console.error("Unable to find schedules in URL.");
-    return [];
-  }
-
-  // URL Decode and parse the schedules parameter + validate data type
-  let parsedSchedules;
-  try {
-    parsedSchedules = decodeURIComponent(schedulesParameter);
-    parsedSchedules = JSON.parse(parsedSchedules);
-  } catch (error) {
-    console.error("Failed to parse schedules.", error);
-    return [];
-  }
-
-  if (!Array.isArray(parsedSchedules)) {
-    console.error("Unable to find any schedules.");
-    return [];
-  }
-
-  // Validate each schedule item against the schema and then filter out invalid items
-  parsedSchedules = parsedSchedules
-    .map((scheduleEntry) => {
-      try {
-        return ScheduleSchema.parse(scheduleEntry);
-      } catch (error) {
-        console.error("Invalid schedule provided.", error);
-        return undefined;
-      }
-    })
-    .filter(
-      (scheduleItem): scheduleItem is Schedule => scheduleItem !== undefined
-    );
-
-  return parsedSchedules;
-}
+import { ScheduleSchema, type Schedule } from "$lib/schema/schedule";
+import { ConfigSchema, type Config } from "$lib/schema/config";
 
 /**
  * Fetches and validates the config from the URL parameters.
  * @returns {Config | undefined} The config object passed to Apatite.
  */
-function fetchConfig(): Config | undefined {
+function fetchConfig(urlParameters: URLSearchParams): Config | undefined {
   const configParameter = urlParameters.get("config");
 
   if (!configParameter) return;
@@ -76,7 +30,48 @@ function fetchConfig(): Config | undefined {
   }
 }
 
-const schedules = fetchSchedules();
-const config = fetchConfig();
+/**
+ * Fetches and validates schedules from the URL parameters.
+ * @returns {Schedule[] | undefined} An array of schedules passed to Apatite or undefined if no schedules are found.
+ */
+function fetchSchedules(urlParameters: URLSearchParams): Schedule[] | undefined {
+  // Get value of "schedules" query parameter
+  const schedulesParameter = urlParameters.get("schedules");
+  if (!schedulesParameter) {
+    console.error("Unable to find schedules in URL.");
+    return undefined;
+  }
 
-export { schedules, config };
+  // URL Decode and parse the schedules parameter + validate data type
+  let parsedSchedules;
+  try {
+    parsedSchedules = decodeURIComponent(schedulesParameter);
+    parsedSchedules = JSON.parse(parsedSchedules);
+  } catch (error) {
+    console.error("Failed to parse schedules.", error);
+    return undefined;
+  }
+
+  if (!Array.isArray(parsedSchedules)) {
+    console.error("Unable to find any schedules.");
+    return undefined;
+  }
+
+  // Validate each schedule item against the schema and then filter out invalid items
+  parsedSchedules = parsedSchedules
+    .map((scheduleEntry) => {
+      try {
+        return ScheduleSchema.parse(scheduleEntry);
+      } catch (error) {
+        console.error("Invalid schedule provided.", error);
+        return undefined;
+      }
+    })
+    .filter(
+      (scheduleItem): scheduleItem is Schedule => scheduleItem !== undefined
+    );
+
+  return parsedSchedules.length > 0 ? parsedSchedules : undefined;
+}
+
+export { fetchConfig, fetchSchedules };
