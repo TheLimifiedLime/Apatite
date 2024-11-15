d94d612099b381ac4cd1d37fe48edf7c4a26f41f
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-14 23:30:08 -0600

    feat: pass config and fallback (if present) to page if not redirecting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,28 +1,32 @@
 import type { PageLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 import { fetchConfig, fetchSchedules } from "$lib/fetcher";
-import { filter, calculateScheduleItem } from "$lib/processor";
+import {
+  filter,
+  calculateScheduleItem,
+  calculateFallbackSchedule,
+} from "$lib/processor";
 
 export const load: PageLoad = ({ url }) => {
   const config = fetchConfig(url.searchParams);
 
   // If the fetcher finds no schedules, return null
   const schedules = fetchSchedules(url.searchParams);
-  if (!schedules) return { config: config, schedules: null };
+  if (!schedules) return { config: config, fallback: null };
 
   // If no schedules are found after filtering, return null
   const filteredSchedules = filter(schedules);
-  if (!filteredSchedules) return { config: config, schedules: null };
+  if (!filteredSchedules)
+    return {
+      config: config,
+      fallback: calculateFallbackSchedule(schedules),
+    };
 
   // Determine if there is a schedule item to redirect to
   const scheduleItem = calculateScheduleItem(filteredSchedules);
-  if (!scheduleItem) return { config: config, schedules: null };
+  if (!scheduleItem)
+    return { config: config, fallback: calculateFallbackSchedule(schedules) };
 
   // Redirect to the schedule item's URL
   redirect(302, scheduleItem.url);
-
-  return {
-    config: config,
-    schedules: schedules,
-  };
 };
