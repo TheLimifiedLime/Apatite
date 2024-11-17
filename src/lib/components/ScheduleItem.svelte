8c178db3607746cc2f69228b6d6bdc804e7b7ac3
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-16 22:48:26 -0600

    feat: initial compact layout

---
+++ src/lib/components/ScheduleItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { ScheduleItem } from "$lib/schema/schedule";
+  import { Button, Card } from "m3-svelte";
+  const {
+    item,
+  }: {
+    item: ScheduleItem;
+  } = $props();
+  const { name, url, description } = item;
+</script>
+
+<div class="w-full">
+  <Card type="elevated">
+    <div class="min-h-[230px] grid gap-y-16 grid-rows-[auto_auto_1fr_auto]">
+      <header class="text-headline-large">{name}</header>
+      <p class="text-title-medium text-primary font-medium underline">
+        {url}
+      </p>
+      <p class="text-body-large">
+        {description}
+      </p>
+      <div>
+        <Button
+          type="tonal"
+          on:click={() => {
+            window.location.href = url;
+          }}
+          extraOptions={{ title: `Open ${url}` }}>Open</Button
+        >
+      </div>
+    </div>
+  </Card>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import ScheduleItem from "$lib/components/ScheduleItem.svelte";
 
   let { data }: { data: PageData } = $props();
   let { config, fallback } = data;
 </script>
 
+{#if fallback}
+  <main class="grid grid-cols-1 p-16 gap-y-16">
+    <h2 class="text-display-large text-center py-16">
+      {fallback.name}
+    </h2>
+    {#each fallback.items as item}
+      <ScheduleItem {item} />
+    {/each}
+  </main>
+{:else}
+  <!-- TODO: No fallback UI -->
+  <h1>No valid fallback schedules</h1>
+{/if}
