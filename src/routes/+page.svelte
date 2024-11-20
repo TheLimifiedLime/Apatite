670246adc243bf7757abe13f0ceaa9c65ad89a68
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-19 21:04:30 -0600

    refactor: change headings from h2 to h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,9 +15,9 @@
   <main
     class="grid grid-cols-1 p-16 gap-y-16 medium:grid-cols-2 medium:gap-x-24 expanded:grid-cols-3"
   >
-    <h2 class="text-display-large text-center py-16 col-span-full">
+    <h1 class="text-display-large text-center py-16 col-span-full">
       {fallback.name}
-    </h2>
+    </h1>
     {#each fallback.items as item}
       <ScheduleItem {item} />
     {/each}
@@ -26,7 +26,7 @@
   <main
     class="grid grid-cols-1 grid-rows-[auto_1fr] box-border h-lvh p-16 gap-y-16"
   >
-    <h2 class="text-display-large text-center py-16">Apatite</h2>
+    <h1 class="text-display-large text-center py-16">Apatite</h1>
     <div class="flex flex-col gap-y-16 place-self-center">
       <p class="text-display-small text-center text-on-surface-variant">
         No fallback schedules available
