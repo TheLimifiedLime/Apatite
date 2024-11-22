a1ba5a8386607d0d664399110caf056d95ab8f55
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-21 23:10:17 -0600

    feat(schema): add autoRedirect and redirectItem options to config

--- src/lib/schema/config.ts
+++ src/lib/schema/config.ts
@@ -1,7 +1,20 @@
 import z from "zod";
 
 const ConfigSchema = z
-  .object({})
+  .object({
+    autoRedirect: z
+      .boolean()
+      .default(true)
+      .describe(
+        "Whether to redirect to a schedule item link. If false, a fallback will be forced."
+      ),
+    redirectItem: z
+      .string()
+      .optional()
+      .describe(
+        "The name of a schedule item to redirect to. Useful for creating a link to a specific schedule item."
+      ),
+  })
   .describe("Settings for customizing the Apatite.")
   .optional();
 
