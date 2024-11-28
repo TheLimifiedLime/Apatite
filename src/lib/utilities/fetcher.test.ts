1a48dbb07de8fe481764855d305654d01b2d8683
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-11-27 20:59:40 -0600

    test: add a test for scheduleFetcher

--- src/lib/utilities/fetcher.test.ts
+++ src/lib/utilities/fetcher.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { ConfigSchema, type Config } from "$lib/schema/config";
-import type { Schedule } from "$lib/schema/schedule";
+import { ScheduleSchema, type Schedule } from "$lib/schema/schedule";
 import { fetchConfig, fetchSchedules } from "./fetcher";
+import { generateMock } from "@anatine/zod-mock";
 
 describe("fetch config", () => {
   // * INFO: Mocking could be done with a zod mocking library
@@ -44,5 +45,45 @@ describe("fetch config", () => {
     });
 
     expect(fetchConfig(mock)).toStrictEqual(null);
+  });
+});
+
+describe("fetch schedules", () => {
+  function fixDates(schedules: Schedule[]) {
+    /*
+     * INFO: This is a work around to the zod mocking library not mocking z.string.date() correctly
+     * The expected mock in the "2024-10-01" format isn't returned. See:
+     * https://github.com/anatine/zod-plugins/issues/213
+     * https://github.com/anatine/zod-plugins/issues/234
+     */
+    return schedules.map((schedule) => {
+      if (schedule.dateRangeValid) {
+        schedule.dateRangeValid.start = "2024-10-01" as unknown as Date;
+        schedule.dateRangeValid.end = "2024-11-01" as unknown as Date;
+      }
+
+      if (schedule.items.length > 0) {
+        schedule.items.forEach((item) => {
+          item.startTime = "13:00:00" as unknown as Date;
+          item.endTime = "15:30:00" as unknown as Date;
+        });
+      }
+
+      return schedule;
+    });
+  }
+
+  it("parses the schedules correctly", () => {
+    const data = fixDates(
+      Array.from({ length: 5 }, () => generateMock(ScheduleSchema))
+    );
+
+    const mock = new URLSearchParams({
+      schedules: encodeURIComponent(JSON.stringify(data)),
+    });
+
+    const expectedData = data.map((schedule) => ScheduleSchema.parse(schedule));
+
+    expect(fetchSchedules(mock)).toStrictEqual(expectedData);
   });
 });
